a754887de83b61323a89fa98b1988db3af7690e6
AlessioAbbati <alex_117@live.it>
2023-05-16 16:40:46 +0200

    add bonus

--- vue.js
+++ vue.js
@@ -6,9 +6,12 @@ const app = Vue.createApp({
     },
     methods: {
         getRandomMail() {
-        for (let i = 0; i < 10; i++) 
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverResponse => this.mailList.push(serverResponse.data.response));
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverResponse => this.mailList.push(serverResponse.data.response));
+            };
+            this.mailList = [];
         },
+        
         
 
     },
