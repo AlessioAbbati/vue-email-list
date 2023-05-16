560536925035912546b49a4510d979ef29aa17d9
AlessioAbbati <alex_117@live.it>
2023-05-16 15:54:55 +0200

    add 10 mail

--- vue.js
+++ vue.js
@@ -1,14 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-
+           mailList: [],
         };
     },
     methods: {
+        getRandomMail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverResponse => this.mailList = serverResponse.data.response);
+        }
 
+    },
+    created() { 
+        this.getRandomMail();
     },
 });
 
 
 
 app.mount("#app");
+
